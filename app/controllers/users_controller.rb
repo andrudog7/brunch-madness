965a3a8967d099cd0d00f94348110741ce02a95f
--add <andrewksmoker@gmail.com>
2021-02-05 17:01:44 -0500

    adding user controller info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+    def create
+        user = User.find_or_create_by(:username => params[:username])
+        if user 
+            render json: user.to_json(include: {highest_tips: {except: [:id, :created_at, :updated_at, :duration]}}, only: [:username, :id])
+        else
+            render json: {errors: user.errors}
+        end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
     has_many :scores
+
+    def highest_tips
+        self.scores.order(tips: :desc).limit(5)
+    end
 end
